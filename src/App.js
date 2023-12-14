9ce1d79efdd8f76999e6a08a63b111ff9ff08e4c
gowrisankar0 <gsankar9797@gmail.com>
2023-12-14 11:08:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [counter,setCounter] =useState(0);
+const handelIncrement =()=>{
+
+setCounter(counter+1)
+
 }
 
-export default App;
+
+const handleDecrement = ()=>{
+  setCounter(counter-1)
+}
+
+
+ const handleReset = ()=>{
+  setCounter(0)
+ }
+  return (
+    <div className='container'>
+      <div className='conter'>
+
+     <div className='head'>
+      <h1>{counter===0?"No More Counts":""}</h1>
+      <h1>{counter}</h1>
+     </div>
+        
+
+     <div className='btns'>
+      <button onClick={handelIncrement}>INCREMENT</button>
+      <button onClick={handleDecrement}>DECREMENT</button>
+      <button onClick={handleReset}>RESET</button>
+     </div>
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default App
